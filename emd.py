e66005e806045ad387ae3c5cdaa6fcd56edafdbd
ryan <dyw564@gmail.com>
2015-01-09 17:13:38 +0800

    测试一下看看

--- emd.py
+++ emd.py
@@ -10,53 +10,125 @@ import os, os.path
 
 
 class MDBuilder(object):
-    """
-
-    """
+    """"""
+    skips = ['tornado', ]  # 略过这些不生成文档的目录或文件
 
     def __init__(self, path='.'):
         """"""
         self.path = path
 
-    def builder(self, filename):
+    def reader(self, filename):
         """"""
         docs = ['##FROM: {}\n'.format(filename)]
         record = False
         for line in open(filename, 'r').readlines():
+            # 如果注释在一行内写完，就一三引号开始和结束为判断，内容不为空或者换行符
+            if (line.startswith('"""') or line.startswith("'''")) \
+                and (line.endswith('"""\n') or line.endswith("'''\n")):
+                eline = line.replace('"""', '').replace("'''", '')
+                if eline and eline in ['\n', '\r']:
+                    docs.append(eline)
+                continue
+            # 找到注释开始的那一行，开始记录到docs
             if line.startswith('"""') or line.startswith("'''"):
+                eline = line.replace('"""', '').replace("'''", '')
+                if eline and eline in ['\n', '\r']:
+                    docs.append(eline)
                 record = not record
                 continue
+            # 找到注释结束的那一行，停止记录到docs
             if line.endswith('"""\n') or line.endswith("'''\n"):
+                eline = line.replace('"""', '').replace("'''", '')
+                if eline and eline in ['\n', '\r']:
+                    docs.append(eline)
                 record = not record
                 continue
-            if record
+            # 记录注释到docs
+            if record:
                 docs.append(line)
-        docs.append('######End for file {}\n'.format(filename))
+        docs.append('######END for file {}\n'.format(filename))
         return ''.join(docs)
-
-    def element(self):
-        """"""
-        return os.walk(self.path + os.sep)
 
     def writer(self, content, name, path='', mode='w'):
         """"""
-        md = open(os.path.join(path, name.split('.')[0]) + '.md', mode)
-        md.write(content)
+        if path and not os.path.exists(path):
+            os.mkdir(path)
+        filename = os.path.join(path, name.split('.')[0]) + '.md'
+        if not os.path.exists(filename):
+            os.mknod(filename)
+            replace_doc = content
+        else:
+            replace_doc = self.replacement(filename, name, content)
+        md = open(filename, mode)
+        md.write(replace_doc)
         md.close()
 
-    def checker(self, content, name, path):
-        """"""
-        old_content = []
-        for line in open(os.path.join(path, name), 'r').readlines():
-            if line.startswith('##FROM: {}'.format())
-        raise
+    def replacement(self, filename, name, new):
+        """把对应文档中的内容替换了，自己手写的文档只要不在FROM 和 END 中间，就会继续保留"""
+        docs = []
+        record = False
+        for line in open(filename, 'r').readlines():
+            if line.startswith('##FROM: {}'.format(name)):
+                record = not record
+                docs.append(line)
+                continue
+            if line.endswith('######End for file {}\n'.format(name)):
+                record = not record
+                docs.append(line)
+                continue
+            if record:
+                docs.append(line)
+        old = ''.join(docs)
+        total = open(filename, 'r').read()
+        if old in total:
+            return total.replace(old, new)
+        return total + new
 
-    def factory(self):
+    def worker(self):
         """"""
         for file_list in os.walk(self.path + os.sep):
             for filename in file_list[-1]:
                 if filename == '__init__.py':
-                    self.writer(self.builder(os.path.join(file_list[0], filename)), 'README.md', mode='a')
+                    self.writer(self.reader(os.path.join(file_list[0], filename)), file_list[0] + '_init.md', 'doc')
                 elif filename.endswith('.py'):
-                    self.writer(self.builder(os.path.join(file_list[0], filename)), 'README.md', mode='a')
+                    self.writer(self.reader(os.path.join(file_list[0], filename)), filename, 'doc')
+
+    def readme(self):
+        """"""
+        docs = []
+        record = False
+        for line in open('README.md', 'r').readlines():
+            if line.startswith('#Docs'):
+                record = not record
+                docs.append(line)
+                continue
+            if line.endswith('Over Docs'):
+                record = not record
+                docs.append(line)
+                continue
+            if record:
+                docs.append(line)
+        doc_list = docs[0] + map(self.github_url, os.walk(os.path.join(self.path, 'doc') + os.sep).next()[-1]) + docs[-1]
+        old = ''.join(docs)
+        new = ''.join(doc_list)
+        total = open('README.md', 'r').read()
+        if old in total:
+            replace_doc = total.replace(old, new)
+        else:
+            replace_doc = total + new
+        readme_writer = open('README.md', 'w')
+        readme_writer.write(replace_doc)
+        readme_writer.close()
+
+    def github_url(self, name):
+        """"""
+        try:
+            url = [url for url in open('.git/config', 'r').readlines() if url.startswith('\turl')][0]
+        except:
+            url = '\turl = git@github.com:ryanduan/music_api.git'
+        return '*  [{}](http://github.com/{}/blog/master/doc/{})'.format(
+            name,
+            url.split('=')[-1].split('@')[-1].split(':')[-1].split('.')[0],
+            name)
+
 
